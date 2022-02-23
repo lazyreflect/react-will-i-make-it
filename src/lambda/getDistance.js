4872b91dd41c248c29bcfd3e55483a2adf837475
lazyreflect <joseph.pitluck@gmail.com>
2022-02-23 12:17:24 -0700

    Update getDistance.js

--- src/lambda/getDistance.js
+++ src/lambda/getDistance.js
@@ -1,12 +1,12 @@
 // this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
 // import getDistance from 'geolib/es/getDistance'
-import risopData from './risopData.js'
-import { getDistance } from 'geolib'
+import risopData from "./risopData.js";
+import { getDistance } from "geolib";
 // const [userLatitude, setUserLatitude] = useState(51.5103)
 // const [userLongitude, setUserLongitude] = useState(7.49347)
 
-const userLatitude = 51.5103
-const userLongitude = 7.49347
+const userLatitude = 51.5103;
+const userLongitude = 7.49347;
 
 // const getUserLocation = () => {
 //     if (navigator.geolocation) {
@@ -17,24 +17,21 @@ const userLongitude = 7.49347
 //     }
 //   }
 
-
 export function handler(event, context, callback) {
-    
-    const getClosestLocation = () => {
-        const closestLocation = risopData.reduce((acc, curr) => {
-          const distance = getDistance(
-            { latitude: userLatitude, longitude: userLongitude },
-            { latitude: curr.LATITUDE, longitude: curr.LONGITUDE }
-          )
-          return distance < acc.distance ? { distance, location: curr } : acc
-        }, { distance: Infinity, location: {} })
-        return closestLocation.location
-      };
-    console.log('queryStringParameters', event.queryStringParameters)
-    
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify({ msg: getClosestLocation() }),
-    })
-  }
-  
+  console.log("queryStringParameters", event.queryStringParameters);
+  const closestLocation = risopData.reduce(
+    (acc, curr) => {
+      const distance = getDistance(
+        { latitude: userLatitude, longitude: userLongitude },
+        { latitude: curr.LATITUDE, longitude: curr.LONGITUDE }
+      );
+      return distance < acc.distance ? { distance, location: curr } : acc;
+    },
+    { distance: Infinity, location: {} }
+  );
+  console.log('closest location data', closestLocation)
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ msg: closestLocation.location }),
+  });
+}
