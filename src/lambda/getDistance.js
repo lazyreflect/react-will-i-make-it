cf34d64fcf51553ba25d71768d81695aa69334a4
lazyreflect <joseph.pitluck@gmail.com>
2022-02-23 12:26:42 -0700

    Update getDistance.js

--- src/lambda/getDistance.js
+++ src/lambda/getDistance.js
@@ -1,37 +1,19 @@
-// this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
-// import getDistance from 'geolib/es/getDistance'
-import risopData from "../risopData.js";
-import { getDistance } from "geolib";
-// const [userLatitude, setUserLatitude] = useState(51.5103)
-// const [userLongitude, setUserLongitude] = useState(7.49347)
+import { getDistance } from 'geolib'
+const userLatitude = 51.5103
+const userLongitude = 7.49347
 
-const userLatitude = 51.5103;
-const userLongitude = 7.49347;
-
-// const getUserLocation = () => {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(position => {
-//         setUserLatitude(position.coords.latitude)
-//         setUserLongitude(position.coords.longitude)
-//       })
-//     }
-//   }
 
 export function handler(event, context, callback) {
-  console.log("queryStringParameters", event.queryStringParameters);
-  const closestLocation = risopData.reduce(
-    (acc, curr) => {
-      const distance = getDistance(
+    
+    const distance = getDistance(
         { latitude: userLatitude, longitude: userLongitude },
-        { latitude: curr.LATITUDE, longitude: curr.LONGITUDE }
-      );
-      return distance < acc.distance ? { distance, location: curr } : acc;
-    },
-    { distance: Infinity, location: {} }
-  );
-  console.log('closest location data', closestLocation)
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ msg: 'closestLocation.location' }),
-  });
-}
+        { latitude: "51° 31' N", longitude: "7° 28' E" }
+    );
+    console.log('queryStringParameters', event.queryStringParameters)
+    console.log(distance)
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({ msg: `${distance} meters` }),
+    })
+  }
+  
