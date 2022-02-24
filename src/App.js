a3511af14a05af0e46730f2f739b1b4eb3e0f6da
lazyreflect <joseph.pitluck@gmail.com>
2022-02-23 17:11:25 -0700

    functional app

--- src/App.js
+++ src/App.js
@@ -1,57 +1,98 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
-import { getDistance } from 'geolib'
-import { risopData } from './risopData'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getDistance } from "geolib";
+import { risopData } from "./risopData";
 
 // const getUserLocation = () => {
 //   navigator.geolocation.getCurrentPosition(position => {
 //     this.setState({ userLatitude: position.coords.latitude, userLongitude: position.coords.longitude })
 //   })
 // }
-
-class LambdaDemo extends Component {
+class CoordinatesForm extends Component {
   constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null, userLatitude: 35.106766, userLongitude: -106.629181}
+    super(props);
+    this.state = {
+      loading: false,
+      msg: null,
+      userLatitude: 35.106766,
+      userLongitude: -106.629181,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    event.preventDefault();
 
-  handleClick = api => e => {
-    e.preventDefault()
-    const getClosestLocation = risopData.reduce((acc, curr) => {
-      const distance = getDistance(
-          { latitude: this.state.userLatitude, longitude: this.state.userLongitude },
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleClickGetDistance = (api) => (e) => {
+    e.preventDefault();
+    const getClosestLocation = risopData.reduce(
+      (acc, curr) => {
+        const distance = getDistance(
+          {
+            latitude: this.state.userLatitude,
+            longitude: this.state.userLongitude,
+          },
           { latitude: curr.LATITUDE, longitude: curr.LONGITUDE }
-      )
-      if (distance < acc.distance) {
-          return { distance, location: curr }
-      }
-      return acc
-  }, { distance: Infinity, location: {} })
-  
-  console.log(getClosestLocation)
-    this.setState({ loading: false, msg: `${getClosestLocation.distance * 0.000621}` })
-   
-  }
-  //   this.setState({ loading: true })
-  //   fetch("/.netlify/functions/" + api)
-  //     .then(response => response.json())
-  //     .then(json => this.setState({ loading: false, msg: json.msg }))
-  //     .catch(json => this.setState({ loading: false, msg: 'API not working' }))
-  // }
+        );
+        if (distance < acc.distance) {
+          return { distance, location: curr };
+        }
+        return acc;
+      },
+      { distance: Infinity, location: {} }
+    );
+
+    console.log(getClosestLocation);
+    this.setState({
+      loading: false,
+      msg: `You are ${getClosestLocation.distance * 0.000621} miles away from ${getClosestLocation.location.NAME}, a ${getClosestLocation.location.SUBCLASS} in ${getClosestLocation.location.COUNTY}, ${getClosestLocation.location.ST} `,
+    });
+  };
 
   render() {
-    const { loading, msg } = this.state
+    const { loading, msg } = this.state;
 
     return (
-      <p>
-        <button onClick={this.handleClick("getDistance")}>{loading ? "Getting distance..." : "Get Distance"}</button>
+      <form>
+        <label>
+          Latitude:
+          <input
+            name="userLatitude"
+            type="text"
+            value={this.state.userLatitude}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Longitude:
+          <input
+            name="userLongitude"
+            type="text"
+            value={this.state.userLongitude}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <br />
+        <button onClick={this.handleClickGetDistance("getDistance")}>
+          {loading ? "Getting distance..." : "Show me"}
+        </button>
         <br />
         <span>{msg}</span>
-      </p>
-    )
+      </form>
+      
+    );
   }
 }
 
@@ -61,14 +102,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Will you make it?
-          </p>
-          <LambdaDemo />
+          <p>Nuclear War Target Finder</p>
+          <p>Are you far enough away? (U.S. only)</p>
+          <CoordinatesForm />
         </header>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
